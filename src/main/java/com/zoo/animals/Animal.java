9e6cae493c58deb9891480898444d95e401b045a
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-14 15:01:52 +0200

    added superclass Animal; added constructor Animal with name, age, and weight; added methods eat, sleep, makeSound, displayInfo

--- src/main/java/com/zoo/animals/Animal.java
+++ src/main/java/com/zoo/animals/Animal.java
@@ -1,4 +1,37 @@
 package com.zoo.animals;
 
 public class Animal {
+    protected String name;
+    protected int age;
+    protected int weight;
+
+    public Animal(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    private int energyLevel;
+
+    // збільшує енергію на 20
+    public void eat() {
+        System.out.println("");
+    }
+
+    // збільшує енергію на 30
+    public void sleep() {
+        System.out.println("");
+    }
+
+    // виводить загальний звук
+    protected void makeSound() {
+        System.out.println("");
+    }
+
+    // виводить інформацію про тварину
+    public void displayInfo() {
+        System.out.println("");
+    }
+
+    // публічні Геттери для всіх полів
 }
