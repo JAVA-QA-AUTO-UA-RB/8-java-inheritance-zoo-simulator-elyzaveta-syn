ee802474fecbd7fe63fac8fdcc99cd6d4ffbce55
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-20 17:52:55 +0200

    added method fly

--- src/main/java/com/zoo/animals/Bird.java
+++ src/main/java/com/zoo/animals/Bird.java
@@ -1,6 +1,6 @@
 package com.zoo.animals;
 
-public class Bird  extends Animal {
+public class Bird extends Animal {
     public Bird(String name, int age, int weight) {
         super(name, age, weight);
     }
@@ -11,5 +11,10 @@ public class Bird  extends Animal {
     public void makeSound() {
         System.out.println("Sing");
         super.makeSound();
+    }
+
+    public void fly() {
+        System.out.println(name + " is flying.");
+        decreaseEnergy(15);
     }
 }
