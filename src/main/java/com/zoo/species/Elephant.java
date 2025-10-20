c80420ae8ff571d380f32b9308c0bbc38de15b11
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-20 19:49:08 +0200

    added sub-class Elephant, overwrited method makeSound, and added method spraySelf

--- src/main/java/com/zoo/species/Elephant.java
+++ src/main/java/com/zoo/species/Elephant.java
@@ -1,4 +1,18 @@
 package com.zoo.species;
+import com.zoo.animals.Mammal;
 
-public class Elephant {
+public class Elephant extends Mammal {
+    public Elephant(String name, int age, int weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Труба-ба-ба!");
+        super.makeSound();
+    }
+
+    public void spraySelf() {
+        System.out.println(name + " is spraying itself.");
+    }
 }
