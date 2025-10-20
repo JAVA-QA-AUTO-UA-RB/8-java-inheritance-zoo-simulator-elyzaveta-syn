144867c5f160858fcf97e66d089058eac6123c2e
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-20 19:55:44 +0200

    added sub-class Penguin, overwrited methods makeSound, and fly, added method swim

--- src/main/java/com/zoo/species/Penguin.java
+++ src/main/java/com/zoo/species/Penguin.java
@@ -1,4 +1,25 @@
 package com.zoo.species;
+import com.zoo.animals.Bird;
 
-public class Penguin {
+public class Penguin extends Bird {
+    public Penguin(String name, int age, int weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Кря-кря!");
+        super.makeSound();
+    }
+
+    public void swim() {
+        System.out.println(name + " is swimming.");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Пінгвін не літає, але плаває");
+        swim();
+    }
+
 }
