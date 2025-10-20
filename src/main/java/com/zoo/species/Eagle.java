771f6ec7e66d160875282f7d71eff12997a3f249
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-20 19:01:54 +0200

    added sub-class Eagle, overwrited methods makeSound, and fly

--- src/main/java/com/zoo/species/Eagle.java
+++ src/main/java/com/zoo/species/Eagle.java
@@ -1,4 +1,20 @@
 package com.zoo.species;
+import com.zoo.animals.Bird;
 
-public class Eagle {
+public class Eagle extends Bird {
+    public Eagle(String name, int age, int weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Кріі-кріі!");
+        super.makeSound();
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(name + " is flying.");
+        decreaseEnergy(20);
+    }
 }
