dd6ad2f85e343dc2d2895d17ceaa71aa01fa1fcb
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-20 19:46:08 +0200

    added sub-class Lion, overwrited methods makeSound, and added method hunt

--- src/main/java/com/zoo/species/Lion.java
+++ src/main/java/com/zoo/species/Lion.java
@@ -1,4 +1,19 @@
 package com.zoo.species;
+import com.zoo.animals.Mammal;
 
-public class Lion {
+public class Lion extends Mammal {
+    public Lion(String name, int age, int weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Рррр!");
+        super.makeSound();
+    }
+
+    public void hunt(){
+        System.out.println(name + " is hunting");
+        decreaseEnergy(25);
+    }
 }
