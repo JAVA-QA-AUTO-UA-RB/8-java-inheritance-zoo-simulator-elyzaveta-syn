09d55c84059c161cd49da9f99e32f12bb38dd2d4
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-26 12:28:58 +0100

    changed name of harpyEagle2; added greeting System.out.println, zoo scene with zookeeper dialogue, and animal behaviors (hunt, eat, sleep, fly, makeSound, spraySelf)

--- src/main/java/com/zoo/ZooSimulator.java
+++ src/main/java/com/zoo/ZooSimulator.java
@@ -20,7 +20,7 @@ public class ZooSimulator {
 
         Eagle harpyEagle0 = new Eagle("Белла", 1, 6);
         Eagle harpyEagle1 = new Eagle("Бруно", 2, 7);
-        Eagle harpyEagle2 = new Eagle("Ібрагім", 3, 7);
+        Eagle harpyEagle2 = new Eagle("Коко", 3, 7);
 
         Penguin emperorPenguin0 = new Penguin("Кінг", 1, 40);
         Penguin emperorPenguin1 = new Penguin("Сонік", 3, 45);
@@ -31,6 +31,34 @@ public class ZooSimulator {
         // - Годування тварин: використання методу feedAnimal().
         // - Гра з тваринами: використання методу playWithAnimal().
         // - Перевірка рівня енергії тварин: використання методу checkAnimalEnergyLevel().
+
+        System.out.println("⁜ Звичний день у зоопарку \"Величне століття\" ⁜");
+        zookeeper0.talk(zooKeeper1.getName());
+        System.out.println("Алекс подивився на відвідувачів та заговорив: \"Вітаємо у нашому зоопарку! Нумо подивимось на наш прайд левів!\"");
+
+        goldenLion2.hunt();
+        goldenLion2.makeSound();
+        goldenLion3.eat();
+        System.out.println("\n\"Цікаво, а де ж решта?\"\n");
+        goldenLion1.sleep();
+        goldenLion0.sleep();
+        System.out.println("\n\"Нумо тепер подивимось на слонів!\"\n");
+        forestElephant0.makeSound();
+        forestElephant0.spraySelf();
+        forestElephant1.eat();
+        System.out.println("\n\"Недалеко від них живе родина молода родина гарпій. Дивіться-но!\"\n");
+        harpyEagle0.fly();
+        harpyEagle0.makeSound();
+        harpyEagle1.eat();
+        harpyEagle2.makeSound();
+        harpyEagle2.eat();
+        System.out.println("\n\"А в іншій частині в нас живе пара пінгвінів.\"\n");
+        emperorPenguin0.eat();
+        emperorPenguin0.makeSound();
+        emperorPenguin1.fly();
+
+
+
         // Виведіть фінальні результати та стани тварин наприкінці дня.
 
     }
