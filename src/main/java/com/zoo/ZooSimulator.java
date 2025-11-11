d60bf5239d6fce60de52b1ccb230d77e061d357a
elyzaveta-syn <vam.fox86@gmail.com>
2025-11-11 20:03:35 +0100

    changed characteristics of animals: added furColour for lions, and elephants, added windSpan for eagles, and penguins; implemented zoo scene with animals, and zookeepers

--- src/main/java/com/zoo/ZooSimulator.java
+++ src/main/java/com/zoo/ZooSimulator.java
@@ -10,63 +10,83 @@ public class ZooSimulator {
 
     public static void main(String[] args) {
 
-        Lion goldenLion0 = new Lion("Гюррем", 7, 200);
-        Lion goldenLion1 = new Lion("Сулейман", 10, 250);
-        Lion goldenLion2 = new Lion("Міхрімах", 3, 150);
-        Lion goldenLion3 = new Lion("Махідевран", 8, 175);
+        Lion goldenLion0 = new Lion("Гюррем", 7, 200, "темно-золоте");
+        Lion goldenLion1 = new Lion("Сулейман", 10, 250, "класично-золоте");
+        Lion goldenLion2 = new Lion("Міхрімах", 3, 150, "світло-золоте");
+        Lion goldenLion3 = new Lion("Махідевран", 8, 175, "класично-золоте");
 
-        Elephant forestElephant0 = new Elephant("Товстопуз", 5, 3000);
-        Elephant forestElephant1 = new Elephant("Бантік-Срантік", 2, 1500);
+        Elephant forestElephant0 = new Elephant("Товстопуз", 5, 3000, "не має хутра");
+        Elephant forestElephant1 = new Elephant("Бантік-Срантік", 2, 1500, "не має хутра");
 
-        Eagle harpyEagle0 = new Eagle("Белла", 1, 6);
-        Eagle harpyEagle1 = new Eagle("Бруно", 2, 7);
-        Eagle harpyEagle2 = new Eagle("Коко", 3, 7);
+        Eagle harpyEagle0 = new Eagle("Белла", 1, 6, 1.5);
+        Eagle harpyEagle1 = new Eagle("Бруно", 2, 7, 2);
+        Eagle harpyEagle2 = new Eagle("Коко", 3, 7, 2.2);
 
-        Penguin emperorPenguin0 = new Penguin("Кінг", 1, 40);
-        Penguin emperorPenguin1 = new Penguin("Сонік", 3, 45);
+        Penguin emperorPenguin0 = new Penguin("Кінг", 1, 40, 1);
+        Penguin emperorPenguin1 = new Penguin("Сонік", 3, 45, 1.2);
 
         ZooKeeper zookeeper0 = new ZooKeeper("Алекс", 21);
         ZooKeeper zooKeeper1 = new ZooKeeper("Олеся", 27);
         ZooKeeper zooKeeper2 = new ZooKeeper("Ксенія", 25);
         ZooKeeper zooKeeper3 = new ZooKeeper("Іван", 31);
 
-        System.out.println("⁜ Звичний день у зоопарку \"Величне століття\" ⁜");
+        System.out.println("⁜ Звичний день у зоопарку \"Величне століття\" ⁜\n");
         zookeeper0.talk(zooKeeper1);
         System.out.println("Алекс подивився на відвідувачів та заговорив: \"Вітаємо у нашому зоопарку! Нумо подивимось на наш прайд левів!\"");
 
         goldenLion2.hunt();
-        goldenLion2.makeSound();
-        goldenLion3.eat();
+        goldenLion3.move();
         System.out.println("\n\"Цікаво, а де ж решта?\"\n");
         goldenLion1.sleep();
         goldenLion0.sleep();
         System.out.println("\n\"Нумо тепер подивимось на слонів!\"\n");
-        forestElephant0.makeSound();
         forestElephant0.spraySelf();
         forestElephant1.eat();
         System.out.println("\n\"Недалеко від них живе родина молода родина гарпій. Дивіться-но!\"\n");
         harpyEagle0.fly();
-        harpyEagle0.makeSound();
-        harpyEagle1.eat();
-        harpyEagle2.makeSound();
-        harpyEagle2.eat();
+        harpyEagle1.uniqueBirdAction();
+        harpyEagle2.play();
         System.out.println("\n\"А в іншій частині в нас живе пара пінгвінів.\"\n");
-        emperorPenguin0.eat();
-        emperorPenguin0.makeSound();
+        emperorPenguin0.uniqueBirdAction();
         emperorPenguin1.fly();
 
         System.out.println("\nДоки відвідувачі пішли на екскурсію, працівники піклуються про тваринок...\n");
         zooKeeper1.feedAnimal(goldenLion0);
         zooKeeper1.feedAnimal(goldenLion1);
-        zooKeeper1.feedAnimal(goldenLion2);
+        zooKeeper1.washAnimal(goldenLion2);
+        goldenLion2.groom();
         zooKeeper1.feedAnimal(goldenLion3);
         zooKeeper2.washAnimal(forestElephant0);
-        zookeeper0.playWithAnimal(forestElephant1);
+        forestElephant0.groom();
         zooKeeper3.feedAnimal(harpyEagle0);
         zooKeeper3.feedAnimal(harpyEagle1);
         zooKeeper3.washAnimal(harpyEagle2);
-        zooKeeper2.playWithAnimal(emperorPenguin0);
-        zooKeeper2.playWithAnimal(emperorPenguin1);
+        harpyEagle2.groom();
+        zooKeeper2.feedAnimal(emperorPenguin0);
+        zooKeeper2.washAnimal(emperorPenguin1);
+        emperorPenguin1.groom();
+
+        System.out.println("\nТакож усі тваринки мають свій власний час — без відвідувачів!\n");
+        forestElephant0.move();
+        forestElephant1.play();
+        forestElephant0.play();
+        forestElephant1.spraySelf();
+        forestElephant0.spraySelf();
+        emperorPenguin0.play();
+        emperorPenguin1.play();
+        emperorPenguin0.uniqueBirdAction();
+        zooKeeper1.playWithAnimal(goldenLion2);
+        goldenLion0.play();
+        goldenLion3.hunt();
+        goldenLion3.eat();
+        goldenLion1.move();
+        goldenLion1.hunt();
+        zooKeeper3.playWithAnimal(harpyEagle0);
+        harpyEagle0.play();
+        zooKeeper3.playWithAnimal(harpyEagle2);
+        harpyEagle2.play();
+        harpyEagle1.uniqueBirdAction();
+        harpyEagle0.uniqueBirdAction();
 
         System.out.println("\nПотім усі відпочивають...\n");
         goldenLion0.sleep();
